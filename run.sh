0e0253aa0fd1cd1875ac51c66231fda8c225e38d
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2022-01-08 01:51:40 +0100

    Improve testing

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
-mpiexec -n 4 python3 main.py
-mpiexec -n 3 python3 main.py
-mpiexec -n 2 python3 main.py
-mpiexec -n 1 python3 main.py
+mpiexec -n 4 python3 main.py &&
+mpiexec -n 3 python3 main.py &&
+mpiexec -n 2 python3 main.py &&
+mpiexec -n 1 python3 main.py &&
+python3 test.py
--- test.py
+++ test.py
@@ -1,9 +1,10 @@
 import numpy as np
+from itertools import combinations
 
 if __name__ == '__main__':
     nproc = 4
-    for p in range(2, nproc+1):
-        interactions_1 = np.load(f'interactions_1.npy')
-        interactions_p = np.load(f'interactions_{p}.npy')
-        ok = np.isclose(interactions_1, interactions_p).sum()
-        print(f'1-{p}: {ok}/{interactions_1.size}')
+    for i, j in combinations(range(1, nproc+1), 2):
+        interactions_i = np.load(f'interactions_{i}.npy')
+        interactions_j = np.load(f'interactions_{j}.npy')
+        ok = np.isclose(interactions_i, interactions_j).sum()
+        print(f'{i}-{j}: {ok}/{interactions_i.size}')
